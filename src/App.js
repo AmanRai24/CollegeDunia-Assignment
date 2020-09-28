0c4bbbb87a9c5deea9d4a6e23bfc8cfb2abd394d
Aman Rai <arai24197@gmail.com>
2020-09-28 23:55:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Container from "./components/Container"
+import InfiniteScroll from 'react-infinite-scroll-component';
+import colleges from "./assets/data/colleges.json";
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:colleges,
+      items:Array.from({length:10}),
+      hasMore:true,
+    };
+  }
+
+  fetchData=()=>{
+    if(this.state.items.length>=this.state.data.colleges.length){
+      this.setState({hasMore:false});
+      return;
+    }
+
+    setTimeout(()=>{
+      this.setState({
+        items:this.state.items.concat(Array.from({length:10})),
+      });
+    },2000)
+  }
+
+  render(){
+    const{data,items,hasMore}=this.state;
+    const style={
+      color:"red",
+      fontSize:24,
+      textAlign: "center"
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Colleges in North India</span>
+      <InfiniteScroll
+        dataLength={items.length}
+        next={this.fetchData}
+        hasMore={hasMore}
+        loader={<span style={{textAlign:"center", fontSize:20}}>Please Wait!!Loading</span>}
+        endMessage={
+          <span style={style}>Nothing To Show..</span>
+        }
+      >
+        {items.map((i,index)=>(
+          <>
+            <Container key={index} data={data.colleges[index]} ></Container>
+          </>
+        ))}
+      </InfiniteScroll>
     </div>
   );
+}
 }
 
 export default App;
