141aa77e6474cf5a1aa86ff276c7e5baf4ed14a1
Aman Rai <arai24197@gmail.com>
2020-09-29 15:19:05 +0530

    Updates

---
+++ precache-manifest.7f54f2ccb4d4eb3feaebea285207ccce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa31b35ce8e9b2320bafa3cc1a0e18a",
+    "url": "/CollegeDunia-Assignment/index.html"
+  },
+  {
+    "revision": "2b69a861e3561d3464ac",
+    "url": "/CollegeDunia-Assignment/static/css/main.98e7f12a.chunk.css"
+  },
+  {
+    "revision": "9e92d0b98f7a37a7ca10",
+    "url": "/CollegeDunia-Assignment/static/js/2.77283df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CollegeDunia-Assignment/static/js/2.77283df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b69a861e3561d3464ac",
+    "url": "/CollegeDunia-Assignment/static/js/main.721b48f6.chunk.js"
+  },
+  {
+    "revision": "1cf25fd8c278f93297b1",
+    "url": "/CollegeDunia-Assignment/static/js/runtime-main.24c90918.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/CollegeDunia-Assignment/static/media/college_02.619ee97e.jpg"
+  }
+]);
